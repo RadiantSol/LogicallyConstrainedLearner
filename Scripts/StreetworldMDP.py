b9ad8c904874b9c7938a242bb8d6601e9adbecb4
Anthony Marchiafava <amarchia@uno.edu>
2023-04-06 16:56:59 -0500

    added LDBA and improved MDP and Obs

--- Scripts/StreetworldMDP.py
+++ Scripts/StreetworldMDP.py
@@ -4,54 +4,46 @@ from Obs import Observation
 # MDP representation from LCRL
 class StreetWorld:
     def __init__(self, client):
+        self.action_space = [
+            "accelerate",
+            "decelerate"
+        ]
         self.client = client
         self.client.setStepping(True)
         self.sim = self.client.getObject('sim')
         self.sim.startSimulation()
 
-        #Manta
+        #Manta/Car
         self.manta_handle = self.sim.getObject('/Manta')
         self.manta_script = self.sim.getScript(self.sim.scripttype_childscript, self.manta_handle)
-
-
         self.current_state = []
     
     def step(self, action):
-        # this will get the child script of the manta and
-        # allow us to control the manta remotely
-        # *** UNTESTED ***
+        # process action
         if action == 'accelerate':
             self.sim.callScriptFunction('controlVehicle', self.manta_script, 1, 0)
         elif action == 'decelerate':
             self.sim.callScriptFunction('controlVehicle', self.manta_script, 0, 0)
+        
+        # execute action
         self.client.step()
 
-        # evaluate result of step
-        observation = self.sim.callScriptFunction('get_observation', self.track_script)
-        # reversing scaled values using given scaler
-        coords = self.scaler.inverse_transform([observation[0][0], observation[0][1]])
-        # round to 0 decimal places just in case
-        coords[0] = math.round(coords[0])
-        coords[1] = math.round(coords[1])
-        carpos = self.swmap[coords[0]][coords[1]]
-        # calculate distance from center of track
-        for i in self.swmap[coords[0]]:
-            if i == 'c':
-                center = self.swmap[coords[0]].index(i)
-        dist = abs(center - coords[1])
-        self.current_state = self.current_state + [dist, math.round(observation[1])]
-        return self.current_state
+        # check for obstacles
+
+        # update agent state
+        self.agent_state = Observation.get_observation(self.sim)
+
+        # return the MDP state
+        mdp_state = self.agent_state
+        self.current_state = mdp_state
+        return mdp_state
     
-    def state_label(state):
-        goal = False
-        offroad = False
-        #this is wrong
-        loc = state
-        if loc == 'g':
-            goal = 1
-        if loc == 'o':
-            offroad = 1
-        return [goal, offroad]
+    def state_label(self, state):
+        #check if in goal
+        if Observation.check_goal(self.sim):
+            return ["goal"]
+        else:
+            return ["road"]
             
     def reset(self):
         self.sim.stopSimulation()
