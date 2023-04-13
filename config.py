c1b3b5ba976044b4667710fbe27a31c3133042e2
Anthony Marchiafava <amarchia@uno.edu>
2023-04-13 10:12:38 -0500

    made display disabled a config paramerter

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 AL_CHOICE = 'ql'
+DISPLAY_DISABLED = False
 
 # Various hyperparameters for the model
 STEP_LENGTH = 10    # number of steps to take in the simulator for each action (CoppeliaSim default sim step is 50ms per step)
